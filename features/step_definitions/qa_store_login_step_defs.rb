98451795baca9e180ed9e123435f6015ad38782a
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-04-01 19:39:11 +0100

    HTML report on tests created

--- features/step_definitions/qa_store_login_step_defs.rb
+++ features/step_definitions/qa_store_login_step_defs.rb
@@ -1,34 +1,34 @@
-# Given(/^User can access the QA login page$/) do
-#   qa_home_page.visit_home_page
-#   qa_registration_page.click_account_link
-#   qa_login_page.click_login_link
-# end
-#
-# And(/^User inputs a valid username$/) do
-#   qa_login_page.input_login_username('faketestQA123')
-# end
-#
-# And(/^User inputs a password$/) do
-#   qa_login_page.input_login_password('x@%slEpdMDe4bg4W')
-# end
-#
-# When(/^User clicks login button$/) do
-#    qa_login_page.click_submit_button
-# end
-#
-# Then(/^User is taken to profile page$/) do
-#    expect(current_url).to eq('http://store.demoqa.com/wp-admin/profile.php')
-# end
-#
-# Given(/^User inputs an invalid (.*)$/) do |password|
-#   qa_login_page.input_login_username('faketestQA123')
-#   qa_login_page.input_login_password(password)
-# end
-#
-# Then(/^User recieves the correct error message (.*)$/) do |error|
-#   expect(qa_login_page.error_message).to eq(error)
-# end
+Given(/^User can access the QA login page$/) do
+  qa_home_page.visit_home_page
+  qa_registration_page.click_account_link
+  qa_login_page.click_login_link
+end
 
-# Given(/^User inputs a valid password$/) do
-#   qa_login_page.input_login_password('x@%slEpdMDe4bg4W')
-# end
+And(/^User inputs a valid username$/) do
+  qa_login_page.input_login_username('faketestQA123')
+end
+
+And(/^User inputs a password$/) do
+  qa_login_page.input_login_password('x@%slEpdMDe4bg4W')
+end
+
+When(/^User clicks login button$/) do
+   qa_login_page.click_submit_button
+end
+
+Then(/^User is taken to profile page$/) do
+   expect(current_url).to eq('http://store.demoqa.com/wp-admin/profile.php')
+end
+
+Given(/^User inputs an invalid (.*)$/) do |password|
+  qa_login_page.input_login_username('faketestQA123')
+  qa_login_page.input_login_password(password)
+end
+
+Then(/^User recieves the correct error message (.*)$/) do |error|
+  expect(qa_login_page.error_message).to eq(error)
+end
+
+Given(/^User inputs a valid password$/) do
+  qa_login_page.input_login_password('x@%slEpdMDe4bg4W')
+end
--- features/step_definitions/qa_store_register_step_defs.rb
+++ features/step_definitions/qa_store_register_step_defs.rb
@@ -1,31 +1,31 @@
-# Given(/^User can access the QA register homepage$/) do
-#   qa_home_page.visit_home_page
-#   qa_registration_page.click_account_link
-#   qa_registration_page.click_register_link
-# end
+Given(/^User can access the QA register homepage$/) do
+  qa_home_page.visit_home_page
+  qa_registration_page.click_account_link
+  qa_registration_page.click_register_link
+end
 
-# And(/^User inputs a username$/) do
-#   qa_registration_page.input_username('fake4tsdsh3jhrers')
-# end
-#
-# And(/^User inputs an email$/) do
-#   qa_registration_page.input_email('faktest34sdsdsd79798@hotmail.com')
-# end
-#
-# When(/^User clicks register button$/) do
-#   qa_registration_page.click_submit_button
-# end
-#
-# Then(/^Registration complete message appears$/) do
-#   expect(qa_registration_page.register_message).to eq('Registration complete. Please check your email.')
-# end
-#
-# Given(/^User inputs a (.*) which is already taken$/) do |username|
-#   qa_registration_page.input_username(username)
-#   qa_registration_page.input_email('testtestdsfsdsdsds@hotmail.com')
-#   qa_registration_page.click_submit_button
-# end
-#
-# Then(/^I receive the corresponding error (.*)$/) do |error|
-#   expect(qa_registration_page.username_taken_message).to eq(error)
-# end
+And(/^User inputs a username$/) do
+  qa_registration_page.input_username('fake4t4ffdsjhrers')
+end
+
+And(/^User inputs an email$/) do
+  qa_registration_page.input_email('faktestfdsfseeee@hotmail.com')
+end
+
+When(/^User clicks register button$/) do
+  qa_registration_page.click_submit_button
+end
+
+Then(/^Registration complete message appears$/) do
+  expect(qa_registration_page.register_message).to eq('Registration complete. Please check your email.')
+end
+
+Given(/^User inputs a (.*) which is already taken$/) do |username|
+  qa_registration_page.input_username(username)
+  qa_registration_page.input_email('testte454trerdds@hotmail.com')
+  qa_registration_page.click_submit_button
+end
+
+Then(/^I receive the corresponding error (.*)$/) do |error|
+  expect(qa_registration_page.username_taken_message).to eq(error)
+end
